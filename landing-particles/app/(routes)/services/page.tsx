fdb4f78fb70ac43b86d6757aad5bf90fea83f749
Kenneth E. Puliche C <kennethdevpc@gmail.com>
2024-12-18 12:30:01 -0500

    ajustes css

--- landing-particles/app/(routes)/services/page.tsx
+++ landing-particles/app/(routes)/services/page.tsx
@@ -9,7 +9,7 @@ const ServicesPage = () => {
       <TransitionPage />
       <CircleImage />
       <AvatarServices />
-      <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
+      <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2 mb-20">
         <div className="max-w-[450px] mt-20 pt-10 md:mt-0">
           <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">
             Technologies <span className="font-bold text-secondary"> Tools</span>
@@ -21,7 +21,7 @@ const ServicesPage = () => {
         </div>
 
         {/* SLIDER */}
-        <div>{<SliderServices />}</div>
+        <div className="mb-20">{<SliderServices />}</div>
       </div>
     </div>
   );
